e55f2bd2706f58db12498f421aa03945a02eff23
QUangggngu <96270743+quangladn@users.noreply.github.com>
2022-05-15 16:50:52 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let data = [];
+var a = 1
 const addMovie = (ev)=>{
     ev.preventDefault();
     let movie = {
         id: Date.now(),
+        index:document.getElementById('index').value,
         time: new Date(),
         name: document.getElementById('name').value,
         year: document.getElementById('yr').value
